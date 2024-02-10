97d472b8762d22ee613aafe4dcda3674d8113491
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2024-02-10 22:48:11 +0100

    init to load lazy

--- init.lua
+++ init.lua
@@ -1,5 +1,18 @@
 -- require "vim"
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")

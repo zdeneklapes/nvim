e53a045cdcb08ab358d4b5dcc9d6abbeae62e690
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2024-02-11 18:20:35 +0100

    fzf

--- init.lua
+++ init.lua
@@ -116,6 +116,28 @@ local plugins= {
     {
         "nvim-telescope/telescope.nvim",
         tag = '0.1.5',
+        -- dependencies = {
+        --     "nvim-telescope/telescope-fzf-native.nvim",
+        --     build = "make",
+        --     config = function()
+        --         require("telescope").load_extension("fzf")
+        --     end,
+        -- },
+        dependencies = {
+            'nvim-telescope/telescope-fzf-native.nvim', 
+            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
+        },
+        -- opts = {
+        --     defaults = {
+        --         path_display = {shorten = 3},
+        --         prompt_prefix = "> ",
+        --     },
+        --     extensions_list = {
+        --         "fzf", 
+        --         "terms", 
+        --         "themes"
+        --     },
+        -- },
     },
     
     {
@@ -156,6 +178,21 @@ require("lazy").setup(plugins, opts)
 -- require("telescope").load_extension("recent_files")
 -- Map a shortcut to open the picker.
 -- vim.api.nvim_set_keymap("n", "<Leader><Leader>", [[<cmd>lua require('telescope').extensions.recent_files.pick()<CR>]], {noremap = true, silent = true})
+
+require('telescope').setup {
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
 -- vim.keymap.set('n', '<leader>t', , {})

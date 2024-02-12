b3eb089dd14b2ca6375bf033f4a38f430dfd36ff
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2024-02-12 03:32:26 +0100

    lualine

--- init.lua
+++ init.lua
@@ -99,6 +99,37 @@ local plugins= {
     },
 
     {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup()
+        end
+    },
+--                options = {
+--                    theme = 'catppuccino',
+--                    section_separators = {'', ''},
+--                    component_separators = {'', ''},
+--                },
+--                sections = {
+--                    lualine_a = {'mode'},
+--                    lualine_b = {'branch'},
+--                    lualine_c = {'filename'},
+--                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+--                    lualine_y = {'progress'},
+--                    lualine_z = {'location'}
+--                },
+--                inactive_sections = {
+--                    lualine_a = {},
+--                    lualine_b = {},
+--                    lualine_c = {'filename'},
+--                    lualine_x = {'location'},
+--                    lualine_y = {},
+--                    lualine_z = {}
+--                },
+--                tabline = {},
+--                extensions = {}
+
+    {
       'rmagatti/session-lens',
           requires = {'rmagatti/auto-session', 'nvim-telescope/telescope.nvim'},
           config = function()

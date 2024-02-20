d7b5f8c38876da35fc33657ef458d1a878bcc7f8
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2024-02-20 01:30:30 +0100

    Error: nvim-treesitter (when used 'nvim .')

--- init.lua
+++ init.lua
@@ -377,12 +377,12 @@ vim.keymap.set("n", "<leader>tht", builtin.help_tags, { desc = "Telescope help t
 vim.keymap.set("n", "<leader>tlg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>tts", builtin.treesitter, { desc = "Telescope treesitter" })
 vim.keymap.set("n", "<leader>tdd", builtin.diagnostics, { desc = "Telescope diagnostics" })
-vim.keymap.set(
-	"n",
-	"<leader>ttt",
-	":Telescope telescope-tabs list_tabs<CR>",
-	{ noremap = true, silent = true, desc = "Telescope telescope-tabs list_tabs" }
-)
+-- vim.keymap.set(
+-- 	"n",
+-- 	"<leader>ttt",
+-- 	":Telescope telescope-tabs list_tabs<CR>",
+-- 	{ noremap = true, silent = true, desc = "Telescope telescope-tabs list_tabs" }
+-- )
 
 require("catppuccin").setup()
 
@@ -397,34 +397,49 @@ setup_colorscheme()
 require("nvim-autopairs").setup()
 
 require("nvim-treesitter.configs").setup({
-	modules = {},
-
-	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
-
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = true,
-
-	-- List of parsers to ignore installing (or "all")
-	ignore_install = { "javascript" },
-
-	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-	-- parser_install_dir = "/some/path/to/store/parsers",
-	-- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
+	-- modules = {},
+	ensure_installed = {
+		"c",
+		"lua",
+		"vim",
+		"vimdoc",
+		"query",
+		"python",
+		"json",
+		"yaml",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"svelte",
+		"php",
+		"go",
+		"rust",
+		"graphql",
+		"dockerfile",
+		"bash",
+		"lua",
+		"json",
+		"yaml",
+		"html",
+		"css",
+		"vim",
+		"typescript",
+		"javascript",
+		"svelte",
+		"php",
+		"go",
+		"rust",
+		"graphql",
+		"dockerfile",
+		"bash",
+	},
+	-- sync_install = false,
+	-- auto_install = true,
+	-- ignore_install = { "javascript" },
 	highlight = {
 		enable = true,
-
-		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-		-- the name of the parser)
-		-- list of language that will be disabled
-		-- disable = { "c", "rust" },
-		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
 		disable = function(_, buf)
 			local max_filesize = 100 * 1024 -- 100 KB
 			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -432,13 +447,12 @@ require("nvim-treesitter.configs").setup({
 				return true
 			end
 		end,
-
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
 	},
+	--   highlight = {
+	--   enable = true,         -- false will disable the whole extension
+	--   disable = { "bash" },  -- list of language that will be disabled
+	-- },
 })
 
 vim.g.loaded_netrw = 1 -- disable netrw at the very start of your init.lua
